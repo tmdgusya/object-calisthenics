53a564c51bfdc28cd09056773fb995838f23a517
roach <tmdgusya@naver.com>
2021-11-26 09:35:25 +0900

    Only One Level Of Indentation Per Method

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -2,14 +2,24 @@ public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j && (nums[i] + nums[j]) == target) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    return answer;
-                }
-            }
+        for (int firstIndex = 0; firstIndex < nums.length; firstIndex++) {
+            answer = fixedSecondIndex(nums, target, firstIndex, answer);
+        }
+        return answer;
+    }
+
+    public int[] fixedSecondIndex(int[] nums, int target, int firstIndex, int[] answer) {
+        for (int secondIndex = 0; secondIndex < nums.length; secondIndex++) {
+            answer = getIndexOfSumOfTarget(nums, target, firstIndex, secondIndex, answer);
+        }
+        return answer;
+    }
+
+    public int[] getIndexOfSumOfTarget(int[] nums, int target, int firstIndex, int secondIndex, int[] answer) {
+        if (firstIndex != secondIndex && (nums[firstIndex] + nums[secondIndex]) == target) {
+            answer[0] = firstIndex;
+            answer[1] = secondIndex;
+            return answer;
         }
         return answer;
     }
